c5666732cb0b6b4341768649344d1cc9c8ca4ab2
sokolowski <slawomir.sokolowski@onet.eu>
2020-11-18 12:50:50 +0100

    gestures

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -9,10 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentAmount: Angle = .degrees(0)
+    @State private var finalAmount: Angle = .degrees(0)
+
     var body: some View {
         Text("Hello, World!")
+            .rotationEffect(currentAmount + finalAmount)
+            .gesture(
+                RotationGesture()
+                    .onChanged { angle in
+                        self.currentAmount = angle
+                    }
+                    .onEnded { angle in
+                        self.finalAmount += self.currentAmount
+                        self.currentAmount = .degrees(0)
+                    }
+            )
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
